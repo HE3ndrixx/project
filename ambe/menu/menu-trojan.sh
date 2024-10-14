9d942e0eef1e59f75c5140f85959535edc632883
Hendrixx <levina77a@gmail.com>
2024-10-14 16:07:50 +0300

    HE3ndrixx

---
+++ ambe/menu/menu-trojan.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# System Request : Debian 9+/Ubuntu 18.04+/20+
+# Develovers » 🗽🥷🎮🧑‍💻🔫Cyberpunk🥷🧑‍💻🗽**TOpPLUG**
+# Email      » tuffgamer65@gmail.com
+# telegram   » https://t.me/T_OpPLUG
+# whatsapp   » wa.me/+254112386921
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# 🧑‍💻🏴‍☠️🥷🗽**TOpPLUG script**🧑‍💻🏴‍☠️🥷🗽
+# ==========================================
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting Language to UTF-8
+export LC_ALL='en_US.UTF-8' > /dev/null
+export LANG='en_US.UTF-8' > /dev/null
+export LANGUAGE='en_US.UTF-8' > /dev/null
+export LC_CTYPE='en_US.utf8' > /dev/null
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} ERROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="HE3ndrixx.github.io/project/ambe"
+export Server_URLL="raw.githubusercontent.com/HE3ndrixx/perizinan/main"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther="🧑‍💻HE3ndrixx🧑‍💻☁️☁️☁️☁️🗽TOpPLUG🧑‍💻"
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting IP Address
+export IP=$( curl -sS ipinfo.io/ip )
+
+# // Exporting Network Interface
+export NETWORK_IFACE="$(ip route show to default | awk '{print $5}')"
+
+# // Validate Result ( 1 )
+touch /etc/${Auther}/license.key
+export Your_License_Key="$( cat /etc/${Auther}/license.key | awk '{print $1}' )"
+export Validated_Your_License_Key_With_Server="$( curl -s https://${Server_URLL}/registered.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 1 )"
+if [[ "$Validated_Your_License_Key_With_Server" == "$Your_License_Key" ]]; then
+    validated='true'
+else
+    echo -e "${EROR} License Key Not Valid"
+    exit 1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ $IP == "$( curl -s https://${Server_URLL}/blacklist.txt | cut -d ' ' -f 1 | grep -w $IP | head -n1 )" ]]; then
+    echo -e "${EROR} 403 Forbidden ( Your VPS Has Been Banned )"
+    exit  1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ $Your_License_Key == "$( curl -s https://${Server_URLL} | cut -d ' ' -f 1 | grep -w $Your_License_Key | head -n1)" ]]; then
+    echo -e "${EROR} 403 Forbidden ( Your License Has Been Limited )"
+    exit  1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ 'Standart' == "$( curl -s https://${Server_URLL}/registered.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 8 )" ]]; then 
+    License_Mode='Standart'
+elif [[ Pro == "$( curl -s https://${Server_URLL}/registered.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 8 )" ]]; then 
+    License_Mode='Pro'
+else
+    echo -e "${EROR} Please Using Genuine License !"
+    exit 1
+fi
+
+# // Checking Script Expired
+exp=$( curl -s https://${Server_URLL}/registered.txt | grep -w $Your_License_Key | cut -d ' ' -f 4 )
+now=`date -d "0 days" +"%Y-%m-%d"`
+expired_date=$(date -d "$exp" +%s)
+now_date=$(date -d "$now" +%s)
+sisa_hari=$(( ($expired_date - $now_date) / 86400 ))
+if [[ $sisa_hari -lt 0 ]]; then
+    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
+    echo -e "${EROR} Your License Key Expired ( $sisa_hari Days )"
+    exit 1
+else
+    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
+fi
+clear
+date
+echo ""
+x="ok"
+cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
+if [ "$cekray" = "XRAY" ]; then
+kjj='xray'
+else
+kjj='v2ray'
+fi
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+chck_pid(){
+	PID=`ps -ef |grep -v grep | grep $kjj |awk '{print $2}'`
+	if [[ ! -z "${PID}" ]]; then
+			echo -e "Current status: ${Green_font_prefix} Installed${Font_color_suffix} & ${Green_font_prefix}Running${Font_color_suffix}"
+		else
+			echo -e "Current status: ${Green_font_prefix} Installed${Font_color_suffix} but ${Red_font_prefix}Not Running${Font_color_suffix}"
+		fi
+}
+
+chgck_pid(){
+	PID=`ps -ef |grep -v grep | grep trojan-go |awk '{print $2}'`
+	if [[ ! -z "${PID}" ]]; then
+			echo -e "Current status: ${Green_font_prefix} Installed${Font_color_suffix} & ${Green_font_prefix}Running${Font_color_suffix}"
+		else
+			echo -e "Current status: ${Green_font_prefix} Installed${Font_color_suffix} but ${Red_font_prefix}Not Running${Font_color_suffix}"
+		fi
+}
+
+while true $x != "ok"
+do
+
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[41;1;39m                    ⇱ TROJAN MENU ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+chck_pid
+echo -e "
+[${GREEN}01${NC}] ${color1}•${NC} Create Trojan-GFW Account
+[${GREEN}02${NC}] ${color1}•${NC} Deleting Trojan-GFW Account
+[${GREEN}03${NC}] ${color1}•${NC} Extending Trojan-GFW Account Active Life
+[${GREEN}04${NC}] ${color1}•${NC} Check User Login Trojan-GFW
+
+${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+chgck_pid
+echo -e "
+[${GREEN}05${NC}] ${color1}•${NC} Create Trojan-GO Account
+[${GREEN}06${NC}] ${color1}•${NC} Deleting Trojan-GO Account
+[${GREEN}07${NC}] ${color1}•${NC} Extending Trojan-GO Account Active Life
+[${GREEN}08${NC}] ${color1}•${NC} Check User Login Trojan-GO
+
+[${GREEN}00${NC}] ${color1}• ${NC}Kembali Ke Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
+echo ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[41;1;39m                  ⇱ 🧑‍💻HE3ndrixx🧑‍💻☁️☁️☁️☁️🗽TOpPLUG🧑‍💻 PROJECT ⇲            \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -ne "Select menu : "; read x
+
+case "$x" in 
+   1 | 01)
+   clear
+   addtrojan
+   break
+   ;;
+   2 | 02)
+   clear
+   deltrojan
+   break
+   ;;
+   3 | 03)
+   clear
+   renewtrojan
+   break
+   ;;
+   4 | 04)
+   clear
+   cektrojan
+   break
+   ;;
+   5 | 05)
+   clear
+   addtrgo
+   break
+   ;;
+   6 | 06)
+   clear
+   deltrgo
+   break
+   ;;
+   7 | 07)
+   clear
+   renewtrgo
+   break
+   ;;
+   8 | 08)
+   clear
+   cektrgo
+   break
+   ;;
+   0 | 00)
+   clear
+   menu
+   break
+   ;;
+   *)
+   clear
+esac
+done
+#fim
